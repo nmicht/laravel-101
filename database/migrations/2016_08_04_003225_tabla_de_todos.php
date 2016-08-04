0573160713117d34253a46c5c50ccc31e26685d3
Michelle Torres <michelle.torres@corb.mx>
2016-08-03 19:36:52 -0500

    Agrego migrate creado con artisan

---
+++ database/migrations/2016_08_04_003225_tabla_de_todos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TablaDeTodos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('todos');
+    }
+}
