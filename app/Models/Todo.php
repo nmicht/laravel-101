90b7f8cd6d3f6ef0bdce1a61629fe2d9b7cc97a3
Michelle Torres <michelle.torres@corb.mx>
2016-08-04 20:30:26 -0500

    Usando fillable para el modelo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    //Permitiendo que los campos nombre, estatus y color puedan ser editados con el create de eloquent
+    protected $fillable = ['name','status','color'];
 }
