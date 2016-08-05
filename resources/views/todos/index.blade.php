adbd07de6f9a815e4027e4abd44445b9d7d8dea2
Michelle Torres <michelle.torres@corb.mx>
2016-08-04 20:57:24 -0500

    Dando un poco de diseño a las vistas y creando un formulario

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,16 +1,41 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>Mis ToDos</h1>
-    @foreach ($data as $todo)
+@extends('todos.layout')
+
+@section('contenido')
+  <div class="row">
+  <div class="col-md-5 col-md-offset-3">
+  <h1>Lista de todos</h1>
+    @foreach ($todos as $todo)
       <div class="">
         <input type="checkbox" name="status-{{$todo->id}}" value="">
-        {{$todo->name}}
+        <a href="todos/{{$todo->id}}">{{$todo->name}}</a>
+        <span class="label">{{$todo->color}}</span>
+        <i class="glyphicon glyphicon-trash"></i>
       </div>
     @endforeach
-  </body>
-</html>
+  </div>
+  </div>
+  <div class="row">
+    <div class="col-md-5 col-md-offset-3">
+      <form class="" action="todos" method="post">
+        <div class="form-group">
+          <label for="name">ToDo</label>
+          <input type="text" name="name" value="" class="form-control">
+        </div>
+        <div id="cp2" class="input-group colorpicker-component">
+            <input type="text" value="#00AABB" name="color" class="form-control" />
+            <span class="input-group-addon"><i></i></span>
+        </div>
+        <button type="submit" class="btn">Crear</button>
+
+      </form>
+    </div>
+  </div>
+@stop
+
+@section('scripts')
+  <script>
+      $(function() {
+          $('#cp2').colorpicker();
+      });
+  </script>
+@stop
--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>ToDo site</title>
     <link rel="stylesheet" href="{{asset('css/main.css')}}" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
   <body>
 
