a8f6192c06536107c2484abcc973810af0f1b824
Michelle Torres <michelle.torres@corb.mx>
2016-08-04 20:33:15 -0500

    Creando un layout

---
+++ resources/views/todos/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ToDo site</title>
+    <link rel="stylesheet" href="{{asset('css/main.css')}}" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+
+    @yield('contenido')
+
+    <footer>
+      @yield('footer')
+      Copyright Michelle 2016
+    </footer>
+  </body>
+</html>
