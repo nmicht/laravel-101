d1c2932f30291d200dc6d79b3604da97dd632a11
Michelle Torres <michelle.torres@corb.mx>
2016-08-04 23:35:10 -0500

    Cambiar estatus para que sea un booleano

---
+++ database/migrations/2016_08_05_034730_change_status_to_boolean.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeStatusToBoolean extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->boolean('status')->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            //
+        });
+    }
+}
