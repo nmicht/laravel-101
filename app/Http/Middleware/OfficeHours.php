a4da9232134ba084fc66320b5d5b8d11168ded5e
Michelle Torres <michelle.torres@corb.mx>
2016-08-10 17:23:42 -0500

    Validando hora y arrojando flash message en caso de error

--- app/Http/Middleware/OfficeHours.php
+++ app/Http/Middleware/OfficeHours.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
 use Closure;
 
 class OfficeHours
@@ -18,7 +19,18 @@ class OfficeHours
         //Aquí meteremos la validación que queremos que ocurra antes de pasar al
         //resto del proceso definido en la ruta
 
+        //Validaremos que no se pueda cambiar de estatus el todo a menos que
+        //sean horarios de oficina
+        $hora = Carbon::now()->hour; //Obtengo la hora
+        if ($hora > 18 || $hora < 9) {
 
+            //Arrojo alerta
+            session()->flash('flash_message','Esta acción no se puede realizar, estas fuera de horario');
+            session()->flash('flash_message_type','danger');
+
+            //Regreso a donde venía
+            return back();
+        }
         //Esto permite que continue la ejecución definida en la ruta
         return $next($request);
     }
