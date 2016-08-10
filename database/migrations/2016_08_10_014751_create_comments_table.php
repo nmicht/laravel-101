7796fd444fbd701f7b06e0d13df87dfe8b5221d9
Michelle Torres <michelle.torres@corb.mx>
2016-08-09 21:19:36 -0500

    Creo migrate y lo ejecuto para tener comentarios

---
+++ database/migrations/2016_08_10_014751_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('comment');
+            $table->integer('todo_id')->unsigned()->index(); //Campo para recibir el id del todo
+            $table->timestamps();
+
+            //Agrego el constraint para asegurar la integridad referencial
+            $table->foreign('todo_id')->references('id')->on('todos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
