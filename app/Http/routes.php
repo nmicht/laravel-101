77fa6b39548fad2bf7d28905d8123cc01be41bdf
Michelle Torres <michelle.torres@corb.mx>
2016-08-10 15:47:06 -0500

    Cambio la ruta de login y agrego la vista

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -68,6 +68,6 @@ Route::delete('comments/{comment}','CommentsController@destroy');
 
 
 //------------------- AUTH --------------------------------
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
