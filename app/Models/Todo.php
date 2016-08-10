453d5dccec7d7771e977b6f5d803168016cdb207
Michelle Torres <michelle.torres@corb.mx>
2016-08-09 21:25:07 -0500

    Creo la función para la relación y describo la relación que un todo tiene comentarios

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,12 @@ class Todo extends Model
 {
     //Permitiendo que los campos nombre, estatus y color puedan ser editados con el create de eloquent
     protected $fillable = ['name','status','color'];
+
+    //Creo un método que me permitirá acceder a la relación con los comentarios
+    //como un todo tiene muchos comentarios, el nombre lo pongo en plural
+    public function comments(){
+      //Regreso la relación que tiene con los comentarios
+      return $this->hasMany('\App\Models\Comment');
+
+    }
 }
