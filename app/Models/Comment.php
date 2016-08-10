b217e2ad85f019a613aa52c3782017b19e7b168b
Michelle Torres <michelle.torres@corb.mx>
2016-08-09 21:23:44 -0500

    Creo la función para la relación y describo la relación que un comentario pertenece a un Todo

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,12 @@ class Comment extends Model
 {
     //Agrego los campos que tienen permisos de escritura
     protected $fillable = ['comment','todo_id'];
+
+    //Creo un método que me permitirá acceder a la relación con los todos
+    //como un comentario tiene un todo, el nombre lo deje en singular
+    public function todo(){
+      //Regreso la relación que tiene con los Todo
+      return $this->belongsTo('\App\Models\Todo');
+
+    }
 }
