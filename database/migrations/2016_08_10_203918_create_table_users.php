001eaa4483a9a149ccf0fed9c24ed809d68f721e
Michelle Torres <michelle.torres@corb.mx>
2016-08-10 15:55:15 -0500

    Arreglando detalle que el migrate de usuarios no estaba completo

--- database/migrations/2016_08_10_203918_create_table_users.php
+++ database/migrations/2016_08_10_203918_create_table_users.php
@@ -14,6 +14,11 @@ class CreateTableUsers extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password',60); //Minimo de 60 caracteres
+            $table->rememberToken();
             $table->timestamps();
         });
     }
