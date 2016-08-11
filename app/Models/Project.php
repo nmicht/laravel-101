d3a41fb594b0b6c0661ec99739cb2ee27e9ab876
Michelle Torres <michelle.torres@corb.mx>
2016-08-11 15:18:28 -0500

    Creando relación entre usuarios y proyectos

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,14 @@ class Project extends Model
 {
     //mass
     protected $fillable = ['name','user_id'];
+
+    /**
+     * Método que genera la relación entre proyecto y usuarios
+     * @return query builder
+     */
+    public function manager(){
+        //como segundo parametro le pasamos el id porque manager no es una tabla
+        //y si no lo hago buscara manager_id
+        return $this->belongsTo('\App\User','user_id');
+    }
 }
